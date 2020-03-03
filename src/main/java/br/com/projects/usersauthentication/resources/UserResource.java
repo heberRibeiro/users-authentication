c64cfc717bbf86330b5d9298f6aee3fc9a70efe9
Heber Ribeiro <heber.r@outlook.com>
2020-03-03 18:19:31 -0300

    Connected mongodb with service and repository layer

--- src/main/java/br/com/projects/usersauthentication/resources/UserResource.java
+++ src/main/java/br/com/projects/usersauthentication/resources/UserResource.java
@@ -1,29 +1,26 @@
 package br.com.projects.usersauthentication.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projects.usersauthentication.domain.User;
+import br.com.projects.usersauthentication.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User joao = new User("1", "joao@joao.com", "123");
-		User maria = new User("2", "maria@maria.com", "123");
-		User ana = new User("3", "ana@ana.com", "123");
-		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(joao, maria, ana));
-		
+		List<User> list = userService.findAll();		
 		return ResponseEntity.ok().body(list);		
 	}
 
