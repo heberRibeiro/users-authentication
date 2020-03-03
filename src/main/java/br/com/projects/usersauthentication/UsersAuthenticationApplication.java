e2c6453b490a199fb1e4ca6012f1a86b823baae5
Heber Ribeiro <heber.r@outlook.com>
2020-03-03 17:20:54 -0300

    Created domain class

---
+++ src/main/java/br/com/projects/usersauthentication/UsersAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.usersauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projects/usersauthentication/UsersAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projects.usersauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
